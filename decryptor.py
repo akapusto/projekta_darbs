28cbc9cd9570cfc92c67c919a8b3fbceda3117ae
akapusto <artjomsk3@gmail.com>
2026-05-07 20:16:14 +0300

    finala versija

--- decryptor.py
+++ decryptor.py
@@ -1,8 +1,3 @@
-#
-# Ši programmā dešifrēs iešifrēto failu pie nezināmas atslēgas
-#
-
-
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
@@ -11,10 +6,34 @@ from math import gcd
 import json
 
 thisDir = Path(__file__).parent
+decryptedBooksDir = thisDir / 'decrypted_books'
+decryptedBooksDir.mkdir(exist_ok = True)
+filesInFolder = sum(1 for f in decryptedBooksDir.iterdir() if f.is_file())
+ALPHABET = ['a','b','c','d','e','f','g',
+            'h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u',
+            'v','w','x','y','z']
+def evaluateResult(text, frequencyTable):
+    textFrequencies = {}
+    total = len(text)
+    for c in text:
+        textFrequencies[c] = textFrequencies.get(c,0) + 1
+    score = 0
+    for c in textFrequencies:
+        frequencyIs = textFrequencies[c] / total
+        frequencyShouldBe = float(frequencyTable.get(c, 0))/100
+        score -= abs(frequencyIs - frequencyShouldBe)
+    return score
 
+def gcd_of_distances(lst):
+    g = 0
+    for i in range(len(lst)-1):
+        dist = lst[i+1] - lst[i]
+        g = gcd(g, dist)
+    return g
 def ngramsFromText(text, n):
     #
-    # catches all sequences of n letters
+    # noķer visus burtu savienojumus no n burtiem
     #
     ngrams = {}
     for i in range(len(text)+1-n):
@@ -25,17 +44,17 @@ def ngramsFromText(text, n):
     return ngrams
 def repeatingngrams(ngrams):
     #
-    # catches all letter sequences whith repeats more than once
+    # filtrē n-grammas, kas atkartojas vismaz divas reizes
     #
     repeating_ngrams = {}
     for ngram in ngrams:
         if len(ngrams[ngram])>1:
             repeating_ngrams[ngram] = ngrams[ngram]
-    repeating_ngrams = dict(sorted(repeating_ngrams.items(),key = lambda item: len(item[1]))) # sortes dictionary by element's value's lenght
+    repeating_ngrams = dict(sorted(repeating_ngrams.items(),key = lambda item: len(item[1])))
     return repeating_ngrams
 def differences(lst):
     #
-    # counts all possible differences between 2 elements in given list
+    # skaitļo visas iespējamas starpības starp diviem elementiem sarakstā
     #
     differences = []
     for el1 in lst:
@@ -49,7 +68,12 @@ def keyLenghtCandidates(text):
     #
     ngrams = repeatingngrams(ngramsFromText(text,n=3))
     candidate_list = {}
+    lght = len(ngrams)                            #
+    i=0                                           #
     for ngram in ngrams:
+        i+=1
+        print(i/lght*100, '%')                    #
+        root.update()                             #
         intervals = differences(ngrams[ngram])
         if len(intervals)>1:
             candidate = gcd(*intervals)
@@ -61,26 +85,14 @@ def keyLenghtCandidates(text):
             candidate_list[candidate] +=1
     candidate_list = dict(sorted(candidate_list.items(),key= lambda item: item[1],reverse=True))
     return candidate_list
-def decryptCaesar(text,frequencyTable = None):
-    if not (thisDir/'SymbolFrequencies.json').exists() and frequencyTable == None:
-        print('frequency table wasn\'t found')
-        return
-    if frequencyTable == None:
-        with open(thisDir/'SymbolFrequencies.json',mode='r',encoding='utf-8') as f:
-            frequencyTable = json.load(f)
-    textFrequencies = {}
+def decryptCaesar(text, key):
+    # key in range(26)
+    result = []
     for char in text:
-        if char not in textFrequencies:
-            textFrequencies[char] = 1
-        else:
-            textFrequencies[char] +=1
-    relativeTextFrequencies = {char: textFrequencies[char]/len(text) for char in textFrequencies}
-    relativeTextFrequencies = dict(sorted(relativeTextFrequencies.items(),key = lambda item: item[1],reverse=True))
-    charsText = list(relativeTextFrequencies.keys())
-    charsStatistics = list(frequencyTable.keys())
-    translateTable = {charsText[i]: charsStatistics[i] for i in range(min(len(charsText),len(charsStatistics)))}
-    decryptedText = text.translate(str.maketrans(translateTable))
-    return decryptedText
+        if char in ALPHABET:
+            charIndex = ALPHABET.index(char)
+            result.append(ALPHABET[(charIndex-key) % len(ALPHABET)])
+    return ''.join(result)
 def chooseFile():
     global chosenFile
     chosenFile = filedialog.askopenfilename()
@@ -116,32 +128,37 @@ def kasiskiMethod():
         ttk.Label(frame2,text=f'({candidates[keys[i]]})').grid(column=2,row=i+1)
     frame2.pack()
     process_lbl.destroy()
-    klen = keys[0]
-    ttk.Label(sub,text=f'The most popular key\'s lenght was {keys[0]}.\nIt will be used further for decrypting algorhytm.').pack()
+    klen = keys[1]
+    ttk.Label(sub,text=f'Key\'s lenght is {klen}.').pack()
     ttk.Button(sub,text=f'Ok',command = lambda: sub.destroy())
     startdecryptbtn.pack()
 def decryptVigenere():
-    #
-    # balstoties uz citās programmās izskaitļotas frekvenču tabulas mēģina dešifrēt tekstu
-    #
-    if not (thisDir/'SymbolFrequencies.json').exists():
+    if not (thisDir/'frequencies.json').exists():
         print('frequency table wasn\'t found')
         return
-    with open(thisDir/'frequencies.json',mode='r',encoding='utf-8') as f:
-        frequencyTable = json.load(f)['relative frequencies']
-        frequencyTable = dict(sorted(frequencyTable.items(),key=lambda item: item[1],reverse=True))
-    with open(chosenFile,mode='r',encoding='utf-8') as f:
-        text= f.read()
-    splitted_text = ['' for i in range(klen)]
+    with open(thisDir/'frequencies.json', mode='r', encoding='utf-8') as f:
+        frequencyTable = json.load(f)
+    with open(chosenFile, mode='r', encoding='utf-8') as f:
+        text = f.read()
+    fragments = ['' for i in range(klen)]
     for i in range(len(text)):
-        splitted_text[i%klen]+=text[i]
-    decrypted_splitted_text = []
-    for fragment in splitted_text:
-        decrypted_splitted_text.append(decryptCaesar(fragment,frequencyTable))
-    decrypted_text=''
+        fragments[i % klen] += text[i]
+    decrypted_fragments = []
+    for fragment in fragments:
+        best_score = -100
+        best_text = ""
+        for shift in range(26):
+            candidate = decryptCaesar(fragment, shift)
+            score = evaluateResult(candidate, frequencyTable)
+            if score > best_score:
+                best_score = score
+                best_text = candidate
+        decrypted_fragments.append(best_text)
+    decrypted_text = ""
     for i in range(len(text)):
-        decrypted_text += decrypted_splitted_text[i%klen][i//klen]
-    with open('DECRYPTEDD.txt',mode='w',encoding='utf-8') as f:
+        decrypted_text += decrypted_fragments[i % klen][i // klen]
+    with open(decryptedBooksDir / f'decrypted_book{filesInFolder+1}.txt',
+              mode='w', encoding='utf-8') as f:
         f.write(decrypted_text)
     
 root = tk.Tk()
